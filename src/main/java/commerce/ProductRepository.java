gyuwon/TDDSpringBoot

판매자 상품 목록 조회 API 추가

src/main/java/commerce/ProductRepository.java
@@ -1,5 +1,6 @@
 package commerce;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE p.id = :id")
     Optional<Product> find(UUID id);
+
+    List<Product> findBySellerId(UUID sellerId);
 }

src/main/java/commerce/api/controller/SellerProductsController.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 import commerce.Product;
 import commerce.ProductRepository;
 import commerce.command.RegisterProductCommand;
+import commerce.result.ArrayCarrier;
 import commerce.view.SellerProductView;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,15 +45,35 @@ public record SellerProductsController(ProductRepository repository) {
         return repository
             .find(id)
             .filter(product -> product.getSellerId().equals(sellerId))
-            .map(product -> new SellerProductView(
-                product.getId(),
-                product.getName(),
-                product.getDescription(),
-                product.getPriceAmount(),
-                product.getStockQuantity(),
-                product.getRegisteredTimeUtc()
-            ))
+            .map(SellerProductsController::toView)
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/seller/products")
+    ResponseEntity<?> getProducts(Principal user) {
+        UUID sellerId = UUID.fromString(user.getName());
+        Object[] items = repository
+            .findBySellerId(sellerId)
+            .stream()
+            .sorted(SellerProductsController::compareRegisteredTimesDesc)
+            .map(SellerProductsController::toView)
+            .toArray();
+        return ResponseEntity.ok(new ArrayCarrier<>(items));
+    }
+
+    private static int compareRegisteredTimesDesc(Product a, Product b) {
+        return b.getRegisteredTimeUtc().compareTo(a.getRegisteredTimeUtc());
+    }
+
+    private static SellerProductView toView(Product product) {
+        return new SellerProductView(
+            product.getId(),
+            product.getName(),
+            product.getDescription(),
+            product.getPriceAmount(),
+            product.getStockQuantity(),
+            product.getRegisteredTimeUtc()
+        );
     }
 }

gyuwon/SpringBootTDDStart

src/main/java/commerce/ProductRepository.java
@@ -1,5 +1,6 @@
 package commerce;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE p.id = :id")
     Optional<Product> find(UUID id);
+
+    List<Product> findBySellerIdOrderByRegisteredTimeUtcDesc(UUID sellerId);
 }

src/main/java/commerce/api/controller/SellerProductsController.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 import commerce.Product;
 import commerce.ProductRepository;
 import commerce.command.RegisterProductCommand;
+import commerce.result.ArrayCarrier;
 import commerce.view.SellerProductView;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,15 +45,30 @@ public record SellerProductsController(ProductRepository repository) {
         return repository
             .find(id)
             .filter(product -> product.getSellerId().equals(sellerId))
-            .map(product -> new SellerProductView(
-                product.getId(),
-                product.getName(),
-                product.getDescription(),
-                product.getPriceAmount(),
-                product.getStockQuantity(),
-                product.getRegisteredTimeUtc()
-            ))
+            .map(SellerProductsController::toView)
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/seller/products")
+    ResponseEntity<?> getProducts(Principal user) {
+        UUID sellerId = UUID.fromString(user.getName());
+        SellerProductView[] items = repository
+            .findBySellerIdOrderByRegisteredTimeUtcDesc(sellerId)
+            .stream()
+            .map(SellerProductsController::toView)
+            .toArray(SellerProductView[]::new);
+        return ResponseEntity.ok(new ArrayCarrier<>(items));
+    }
+
+    private static SellerProductView toView(Product product) {
+        return new SellerProductView(
+            product.getId(),
+            product.getName(),
+            product.getDescription(),
+            product.getPriceAmount(),
+            product.getStockQuantity(),
+            product.getRegisteredTimeUtc()
+        );
     }
 }